39a31aa3d5b613b2c9ae8488650a6e674499b3ef
hochstrass <hochstrass@swissonline.ch>
2023-11-29 01:02:39 +0100

    first

---
+++ src/app/_template/template-nav/template-nav.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TemplateNavComponent } from './template-nav.component';
+
+describe('TemplateNavComponent', () => {
+  let component: TemplateNavComponent;
+  let fixture: ComponentFixture<TemplateNavComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TemplateNavComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TemplateNavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/_template/template-nav/template-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-nav',
+  templateUrl: './template-nav.component.html',
+  styleUrls: ['./template-nav.component.scss']
+})
+export class TemplateNavComponent {
+
+}
