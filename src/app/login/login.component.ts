c2a8723cab30eed1150e2f101adb54a9928aaab0
hochstrass <hochstrass@swissonline.ch>
2023-11-29 20:35:57 +0100

    2023-11-29 20-35

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  ngOnInit() {
+    const LoginMai: HTMLInputElement | any = document.getElementById("LoginMai");
+    LoginMai.focus();
+  }
+
+  SetFoc(eleNam: string): void {
+    switch (eleNam) {
+      case 'Mai':
+        const LoginPaw: HTMLInputElement | any = document.getElementById("LoginPaw");
+        LoginPaw.focus();
+        break;
+
+      case 'Paw':
+        const LoginMai: HTMLInputElement | any = document.getElementById("LoginMai");
+        LoginMai.focus();
+        break;
+    }
+  }
+  public Login_Send(): void {
+    console.log("LoginSend");
+    const LoginMai: HTMLInputElement | any = document.getElementById("LoginMai");
+    LoginMai.focus();
+  }
+
+  public DoTab(event?: any): void {
+    console.log(event);
+  }
 }
+
+
